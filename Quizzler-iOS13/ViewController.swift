72581daaf1f58383c8f4489a7e1c36d47f656f53
FischerAlx <fisheralx@icloud.com>
2022-12-12 17:29:47 +0100

    add a lot of new
    a cycle of questions, true false logic, count of right answer, progressbar and etc

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Quizzler-iOS13
-//
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,14 +7,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var questionLabel: UILabel!
     
+    let quiz = [
+        ["Two plus Four is equal to Six", "True"],
+        ["Four minus Two is greater than One", "True"],
+        ["Five plus One is less then Three", "False"],
+    ]
+    
+    var questionNumber = 0
+    var rightAnswerNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        questionLabel.text = ""
-        
+        progressBar.progress = 0
+        updateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let chooseAnswer = sender.currentTitle
+        let rightAnswer = quiz[questionNumber][1]
+        
+        if questionNumber + 1 < quiz.count{
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        //print(rightAnswer, chooseAnswer!)
+        if rightAnswer == chooseAnswer {
+            rightAnswerNumber += 1
+        }
+        updateUI()
+    }
+    
+
+    func updateUI(){
+        //print(rightAnswerNumber, quiz.count)
+        progressBar.progress = Float(rightAnswerNumber)/Float(quiz.count)
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 }
