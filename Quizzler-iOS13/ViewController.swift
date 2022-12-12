6f8324852540fa49e338e9fc0454ba579d7de8d0
FischerAlx <fisheralx@icloud.com>
2022-12-12 21:47:03 +0100

    add question struct

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -8,9 +8,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     
     let quiz = [
-        ["Two plus Four is equal to Six", "True"],
-        ["Four minus Two is greater than One", "True"],
-        ["Five plus One is less then Three", "False"],
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+        
+        
     ]
     
     var questionNumber = 0
@@ -24,7 +35,7 @@ class ViewController: UIViewController {
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         let chooseAnswer = sender.currentTitle
-        let rightAnswer = quiz[questionNumber][1]
+        let rightAnswer = quiz[questionNumber].answer
         
         if questionNumber + 1 < quiz.count{
             questionNumber += 1
@@ -35,14 +46,32 @@ class ViewController: UIViewController {
         if rightAnswer == chooseAnswer {
             rightAnswerNumber += 1
         }
-        updateUI()
+        UIView.animate(withDuration: 0.2) {
+            self.trueButton.backgroundColor = UIColor.clear
+            self.falseButton.backgroundColor = UIColor.clear
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() ) {
+            UIView.animate(withDuration: 0.2) {
+                if rightAnswer == chooseAnswer {
+                    sender.backgroundColor = UIColor.green
+                    self.updateUI()
+                } else {
+                    sender.backgroundColor = UIColor.red
+                    self.updateUI()
+                }
+            } //UIView
+        }
+        
     }
     
 
-    func updateUI(){
+    @objc func updateUI(){
         //print(rightAnswerNumber, quiz.count)
         progressBar.progress = Float(rightAnswerNumber)/Float(quiz.count)
-        questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
     
 }
